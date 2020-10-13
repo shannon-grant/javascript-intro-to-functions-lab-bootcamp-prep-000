d7b43350ce87b659ccad8d093bebc3af5127d7b9
Shannon Grant <grant.shannon90@gmail.com>
2020-10-13 02:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string.isLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string.isUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma"){
     return "I love you, too."
